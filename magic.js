d4902c19b4fe19a0c58548d04644b7d066246e14
AlexeyTGR <ye.alexey@gmail.com>
2022-03-13 13:48:42 +0300

    added game button small fixes

--- magic.js
+++ magic.js
@@ -11,56 +11,63 @@ let oldValuesArr = [];
 let oldValuesStr = '';
 let newValuesArr = [];
 let newValuesStr = '';
+const MATRIX_SIZE = 4;
+const COMMON_RANDOM_VALUE = 2;
+const RARE_RANDOM_VALUE = 4;
+const VALUE_FOR_VICTORY = 8;
+const VICTORY_MESSAGE = 'VICTORY!';
+const DEFEAT_MESSAGE = 'YOU DIED';
+const START_NEW_GAME_QUESTION = 'Would you like to start a new game?'
+let winCondition = false;
 
-nextElement();
-nextElement();
-refresh();
+const newGameButton = document.getElementById('new-game');
 
-document.addEventListener('keydown', function (event) {
-    if (event.code == 'ArrowLeft') {
-        moveLeft();
-        if (checkChanges()) {
-            nextElement();
-            refresh();
-            isWin();
-        } else isDefeat()
+newGameButton.onclick = startNewGame;
+
+
+function startNewGame() {
+    winCondition = false;
+    valueArray.forEach( (item) => {
+        for (let i = 0; i < valueArray.length; i++){
+            item[i] = null;
+        }
+    });
+    nextElement();
+    nextElement();
+    refresh();
+};
+
+function makeGameTurn() {
+ 
+    const isChange = checkChanges();
+    if (isChange) {
+        nextElement();
     }
-    else if (event.code == 'ArrowRight') {
-        moveRight();
-        if (checkChanges()) {
-            nextElement();
-            refresh();
-            isWin();
-        } else isDefeat()
-    }
-    else if (event.code == 'ArrowUp') {
-        moveUp();
-        if (checkChanges()) {
-            nextElement();
-            refresh();
-            isWin();
-        } else isDefeat()
-    }
-    else if (event.code == 'ArrowDown') {
-        moveDown();
-        if (checkChanges()) {
-            nextElement();
-            refresh();
-            isWin();
-        } else isDefeat()
-    }
-});
+    isWin();
+    refresh();
+    if (winCondition) {
+        setTimeout(() => showVictoryMessage(), 500)
+    } 
+    isDefeat();
+}
 
 function checkChanges() {
-    return oldValuesStr === newValuesStr ? false : true
+    return oldValuesStr === newValuesStr ? false : true;
 }
 
 function isWin() {
     valueArray.forEach(function (item) {
         item.forEach(function (element) {
-            if (element == 2048) alert('You WINiamin!')
+            if (element == VALUE_FOR_VICTORY) {
+                winCondition = true;
+            };
         })
     })
+}
+
+function showVictoryMessage() {
+    alert(VICTORY_MESSAGE);
+    startNewGame();
 }
 
 function isDefeat() {
@@ -73,102 +80,94 @@ function isDefeat() {
 
     for (let i = 0; i < valueArray.length; i++){
         for (let j = 0; j < valueArray.length - 1; j++) {
-            if (valueArray[i][j] == valueArray[i][j + 1]) checkMatch = true
-                      
+            if (valueArray[i][j] == valueArray[i][j + 1]) checkMatch = true;
         }
     }
    
-    for (let i = 0; i < valueArray.length - 1; i++){
-        for (let j = 0; j < valueArray.length; j++){
-            if (valueArray[i][j] == valueArray[i+1][j]) checkMatch = true
-            }
+    for (let i = 0; i < valueArray.length - 1; i++) {
+        for (let j = 0; j < valueArray.length; j++) {
+            if (valueArray[i][j] == valueArray[i+1][j]) checkMatch = true;
         }
+    }
 
-    if (!(checkMatch || checkEmptyCell)) return alert('YOU DEAD')
-}
+    if (!(checkMatch || checkEmptyCell)) {
+        alert(DEFEAT_MESSAGE);
+        let question = confirm(START_NEW_GAME_QUESTION);
+        if (question) startNewGame();
+    }
+};
 
 function nextElement() {
-
     let arr = [];
-    let a;
+    let a = null;
     let b = [];
-    for (let i = 0; i < 4; i++) arr.push(i);
+    for (let i = 0; i < valueArray.length; i++) arr.push(i);
 
-    arr.sort(() => Math.random() - 0.5)
-
+    arr.sort(() => Math.random() - 0.5);
     arr.forEach(function (item, index) {
         if (valueArray[item].includes(null)) {
-            a = index
-            return a = arr[a]
+            a = index;
+
+            return a = arr[a];
         }
     })
 
     valueArray[a].forEach(function (item, index) {
-        if (item == null) {
-            b.push(index)
-        }
+        if (item == null) b.push(index);
     })
 
     b.sort(() => Math.random() - 0.5)
     b = b[0]
 
     let num = Math.random()
-    if (num < 0.89) num = 2
-    else num = 4
+    if (num < 0.89) num = COMMON_RANDOM_VALUE;
+    else num = RARE_RANDOM_VALUE;
 
-    valueArray[a][b] = num
-
-}
+    valueArray[a][b] = num;
+};
 
 function moveLeft() {
-
     oldValuesArr = [...valueArray];
     oldValuesStr = oldValuesArr.join('');
 
     valueArray.forEach(function (elem) {
-
         for (let k = 0; k < elem.length; k++) {
             if (elem[k] == null) {
                 elem.splice(k, 1);
-                k--
+                k--;
             }
         }
 
         for (let j = 0; j < elem.length; j++) {
-
-            if (elem[j] == null) elem.splice(j, 1)
+            if (elem[j] == null) elem.splice(j, 1);
             else if (elem[j] == elem[j + 1]) {
                 elem[j] *= 2;
                 elem.splice(j + 1, 1);
             }
         }
 
-        while (elem.length < 4) elem.push(null)
+        while (elem.length < MATRIX_SIZE) elem.push(null);
     })
 
     newValuesArr = [...valueArray];
     newValuesStr = newValuesArr.join('');
-
 };
 
 function moveRight() {
-
     oldValuesArr = [...valueArray];
     oldValuesStr = oldValuesArr.join('');
 
     valueArray.forEach(function (elem) {
-
-        elem.reverse()
+        elem.reverse();
 
         for (let k = 0; k < elem.length; k++) {
             if (elem[k] == null) {
                 elem.splice(k, 1);
-                k--
+                k--;
             }
         }
 
         for (let j = 0; j < elem.length; j++) {
-
             if (elem[j] == null) elem.splice(j, 1)
             else if (elem[j] == elem[j + 1]) {
                 elem[j] *= 2;
@@ -176,36 +175,29 @@ function moveRight() {
             }
         }
 
-        while (elem.length < 4) elem.push(null)
-
-        elem.reverse()
+        while (elem.length < MATRIX_SIZE) elem.push(null)
+        elem.reverse();
     })
 
     newValuesArr = [...valueArray];
     newValuesStr = newValuesArr.join('');
-
 };
 
 function moveUp() {
-
     oldValuesArr = [...valueArray];
     oldValuesStr = oldValuesArr.join('');
 
     for (let i = 0; i < valueArray.length; i++) {
-
         let elem = [];
-
-        valueArray.forEach(function (item) { elem.push(item[i]) })
-
+        valueArray.forEach(function (item) { elem.push(item[i]) });
         for (let k = 0; k < elem.length; k++) {
             if (elem[k] == null) {
                 elem.splice(k, 1);
-                k--
+                k--;
             }
         }
 
         for (let j = 0; j < elem.length; j++) {
-
             if (elem[j] == null) elem.splice(j, 1)
             else if (elem[j] == elem[j + 1]) {
                 elem[j] *= 2;
@@ -213,40 +205,33 @@ function moveUp() {
             }
         }
 
-        while (elem.length < 4) elem.push(null)
+        while (elem.length < MATRIX_SIZE) elem.push(null)
         for (let h = 0; h < valueArray.length; h++) {
             valueArray[h][i] = elem[h];
         }
-
     }
 
     newValuesArr = [...valueArray];
     newValuesStr = newValuesArr.join('');
-
-}
+};
 
 function moveDown() {
-
     oldValuesArr = [...valueArray];
     oldValuesStr = oldValuesArr.join('');
 
     for (let i = 0; i < valueArray.length; i++) {
-
         let elem = [];
-
-        valueArray.forEach(function (item) { elem.push(item[i]) })
-
-        elem.reverse()
+        valueArray.forEach(function (item) { elem.push(item[i]) });
+        elem.reverse();
 
         for (let k = 0; k < elem.length; k++) {
             if (elem[k] == null) {
                 elem.splice(k, 1);
-                k--
+                k--;
             }
         }
 
         for (let j = 0; j < elem.length; j++) {
-
             if (elem[j] == null) elem.splice(j, 1)
             else if (elem[j] == elem[j + 1]) {
                 elem[j] *= 2;
@@ -254,21 +239,19 @@ function moveDown() {
             }
         }
 
-        while (elem.length < 4) elem.push(null)
-        elem.reverse()
+        while (elem.length < MATRIX_SIZE) elem.push(null)
+        elem.reverse();
+
         for (let h = 0; h < valueArray.length; h++) {
             valueArray[h][i] = elem[h];
         }
-
     }
 
     newValuesArr = [...valueArray];
     newValuesStr = newValuesArr.join('');
-
 };
 
 function refresh() {
-
     document.getElementById('cell00').innerText = valueArray[0][0];
     document.getElementById('cell01').innerText = valueArray[0][1];
     document.getElementById('cell02').innerText = valueArray[0][2];
@@ -288,6 +271,25 @@ function refresh() {
     document.getElementById('cell31').innerText = valueArray[3][1];
     document.getElementById('cell32').innerText = valueArray[3][2];
     document.getElementById('cell33').innerText = valueArray[3][3];
+};
 
-}
-
+document.addEventListener('keydown', function (event) {
+    const eventType = event.code;
+    
+    if (eventType== 'ArrowLeft') {
+        moveLeft();
+        makeGameTurn();
+    }
+    else if (eventType == 'ArrowRight') {
+        moveRight();
+        makeGameTurn();
+    }
+    else if (eventType == 'ArrowUp') {
+        moveUp();
+        makeGameTurn();
+    }
+    else if (eventType == 'ArrowDown') {
+        moveDown();
+        makeGameTurn();
+    }
+});
